dd5e661b799b8a0c9d39a1eb7f840c46ab1ad918
webmis <klingsoul@163.com>
2026-01-27 17:59:59 +0800

    基础控制器

--- core/Controller.go
+++ core/Controller.go
@@ -2,13 +2,12 @@ package core
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 )
 
 /* 基础控制器 */
 type Controller struct {
-	// core.Base
+	Base
 }
 
 /* 获取JSON */
@@ -16,9 +15,4 @@ func (Controller) GetJSON(c http.ResponseWriter, data map[string]interface{}) {
 	c.Header().Set("Content-Type", "application/json; charset=utf-8")
 	c.WriteHeader(http.StatusOK)
 	_ = json.NewEncoder(c).Encode(data)
-}
-
-/* 输出到控制台 */
-func (Controller) Print(content ...interface{}) {
-	fmt.Println(content...)
 }
