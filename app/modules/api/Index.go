dd0583a8666da51953dc3a4c0b3db9dd515d4d69
webmis <klingsoul@163.com>
2026-02-04 14:15:56 +0800

    API

--- app/modules/api/Index.go
+++ app/modules/api/Index.go
@@ -16,7 +16,7 @@ func (c *Index) Index(p http.ResponseWriter, q *http.Request) {
 	// 查询
 	m := (&models.User{}).New()
 	m.Columns("id", "uname")
-	data := m.Find("", nil)
+	data := m.Find("")
 	// Redis
 	r := (&core.Redis{}).New("default")
 	r.Set("test", "Go Redis")
