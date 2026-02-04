ce14a6e79a3872eac8fb57fd7a15d0841845c793
webmis <klingsoul@163.com>
2026-02-04 14:06:54 +0800

    命令行

--- cli/main.go
+++ cli/main.go
@@ -1,23 +1,34 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"webmis/app/task"
 )
 
 func main() {
 	// 参数
-	param := os.Args
-	fmt.Println(param[1], param[2])
+	args := os.Args
+	param := map[string]string{}
+	param["c"] = "main"
+	param["a"] = "index"
+	param["p"] = "index"
+	if len(args) > 1 {
+		param["c"] = args[1]
+	}
+	if len(args) > 2 {
+		param["a"] = args[2]
+	}
+	if len(args) > 3 {
+		param["a"] = args[3]
+	}
 	// 任务
 	switch {
-	case param[1] == "socket":
-		if param[2] == "start" {
+	case param["c"] == "socket":
+		if param["a"] == "start" {
 			// (&task.Socket{}).Start()
 		}
-	case param[1] == "logs":
-		if param[2] == "log" {
+	case param["c"] == "logs":
+		if param["a"] == "log" {
 			// (&task.Logs{}).Log()
 		}
 	default:
